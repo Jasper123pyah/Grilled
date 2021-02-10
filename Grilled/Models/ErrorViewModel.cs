152914791a8cb81897c91ce1b013478a9170ab52
Jasper Meiracker <362948@student.fontys.nl>
2021-02-10 10:25:33 +0100

    Initial Version of Grilled

---
+++ Grilled/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Grilled.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
